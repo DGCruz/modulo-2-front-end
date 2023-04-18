ad0734848eb72fbe8c8a97b986dfb1510c729029
Douglas da Cruz <ducatti_cruz@hotmail.com>
2023-04-18 11:29:47 -0300

    adicionando copia da senha

--- 1.1-intro-frontEnd-e-jsAssincrono/gerador-senhas-fortes/src/script.js
+++ 1.1-intro-frontEnd-e-jsAssincrono/gerador-senhas-fortes/src/script.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import clipboard from 'clipboard-copy';
 import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
@@ -6,4 +7,20 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
     const randomPassword = nanoid();
     displayPasswordEl.innerHTML = randomPassword;
+});
+
+
+// displayPasswordEl.addEventListener('click', (event) => {
+//     clipboard(event.target.innerHTML);
+//     alert('Senha copiada!');
+// });
+
+const btnCopiar = document.getElementById('copiar');
+
+btnCopiar.addEventListener('click', () => {
+    if (displayPasswordEl.innerHTML === '...') {
+        return alert('Você precisa gerar uma senha primeiro.');
+    }  
+    clipboard(displayPasswordEl.innerHTML);
+    alert('copiado');
 });
